b8efcd7fdfde43dbcc46fbb0589b4b82f0aa1601
Dmitrii_Korolev <korwork@mail.ru>
2020-04-22 17:28:27 -0400

    publishSubscribe 2 channels subscribe to one via bridge

--- src/main/java/com/example/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/example/springintegration/SpringIntegrationApplication.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.integration.annotation.BridgeFrom;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.file.FileReadingMessageSource;
@@ -25,15 +27,28 @@ import java.util.Scanner;
 public class SpringIntegrationApplication {
     public String INPUT_DIR = "CHANGE_TO_INPUT_DIR";
     public String OUTPUT_DIR = "CHANGE_TO_OUTPUT_DIR";
+    public String OUTPUT_DIR2 = "CHANGE_TO_OUTPUT_DIR_2";
     public String FILE_PATTERN = "*.jpg";
 
     @Bean
+    @BridgeFrom(value = "publishSubscribe")
     public MessageChannel fileChannel() {
         return new DirectChannel();
     }
 
     @Bean
-    @InboundChannelAdapter(value = "fileChannel", poller = @Poller(fixedDelay = "1000"))
+    @BridgeFrom(value = "publishSubscribe")
+    public MessageChannel fileChannel2() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel publishSubscribe() {
+        return new PublishSubscribeChannel();
+    }
+
+    @Bean
+    @InboundChannelAdapter(value = "publishSubscribe", poller = @Poller(fixedDelay = "1000"))
     public MessageSource<File> fileReadingMessageSource() {
         FileReadingMessageSource sourceReader= new FileReadingMessageSource();
         sourceReader.setDirectory(new File(INPUT_DIR));
@@ -45,6 +60,15 @@ public class SpringIntegrationApplication {
     @ServiceActivator(inputChannel= "fileChannel")
     public MessageHandler fileWritingMessageHandler() {
         FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(OUTPUT_DIR));
+        handler.setFileExistsMode(FileExistsMode.REPLACE);
+        handler.setExpectReply(false);
+        return handler;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel= "fileChannel2")
+    public MessageHandler fileWritingMessageHandler2() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(OUTPUT_DIR2));
         handler.setFileExistsMode(FileExistsMode.REPLACE);
         handler.setExpectReply(false);
         return handler;
